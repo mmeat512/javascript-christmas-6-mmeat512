19c51df4d84f2dee92c2db7f9d89dfedc5d0044c
mmeat512 <wndus0958@naver.com>
2023-11-11 21:27:53 +0900

    test: 증정 혜택 테스트 추가

---
+++ __tests__/GiftTest.js
@@ -0,0 +1,29 @@
+import Gift from '../src/domain/Gift';
+
+describe('증정 테스트', () => {
+  test('총주문 금액이 12만원 이상인 경우 샴페인을 증정한다.', () => {
+    // given
+    const TOTAL_ORDER_AMOUNT = 130000;
+
+    // when
+    const gift = new Gift();
+    const result = gift.canReciveChampagne(TOTAL_ORDER_AMOUNT);
+
+    // then
+    expect(result).toBe(true);
+  });
+
+  test.each([
+    [20000, 'santa'],
+    [14000, 'tree'],
+    [9000, 'star'],
+    [2000, undefined],
+  ])('총혜택 금액에 따라 배지를 지급받는다.', (totalBenefitAmount, expected) => {
+    // when
+    const gift = new Gift();
+    const result = gift.getReciveBadge(totalBenefitAmount);
+
+    // then
+    expect(result).toBe(expected);
+  });
+});
