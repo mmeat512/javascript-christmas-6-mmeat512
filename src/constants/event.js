ad70e49b23c8d54a688980b3402c5c40d78a39aa
mmeat512 <wndus0958@naver.com>
2023-11-14 04:32:28 +0900

    feat: 이벤트 플래너 기능 구현

---
+++ src/constants/event.js
@@ -0,0 +1,5 @@
+export const MIN_ORDER_AMOUNT = 10000;
+export const EVENT_RANGE = {
+  startDate: 1,
+  endDate: 31,
+};
---
+++ src/service/EventPlanner.js
@@ -0,0 +1,57 @@
+import OutputView from '../view/OutputView.js';
+import Benefit from '../domain/Benefit.js';
+
+class EventPlanner {
+  #visitDate;
+  #order;
+
+  constructor(visitDate, order) {
+    this.#visitDate = visitDate;
+    this.#order = order;
+  }
+
+  getEventPlanner(eventRagne, minOrderAmount) {
+    const orders = this.#order.generateOrdersIncludesPrice();
+    const totalPrice = this.#order.getTotalPrice();
+
+    OutputView.printMenu(orders);
+    OutputView.printTotalAmountBeforeDiscount(totalPrice);
+
+    if (
+      this.#order.isOverMinOrderAmount(minOrderAmount) &&
+      this.#visitDate.isWithinDateRange(eventRagne)
+    ) {
+      this.applyEventBenefit(orders, totalPrice);
+    } else {
+      this.noEventBenefit(totalPrice);
+    }
+  }
+
+  noEventBenefit(totalPrice) {
+    const benefit = new Benefit();
+    OutputView.printGiftMenu();
+    OutputView.pritnBenefitDetails();
+    OutputView.printTotalBenefitAmount();
+    OutputView.printTotalAmountAfterDiscount(totalPrice);
+    OutputView.printEventBadge(benefit.getBadgeDetail(0));
+  }
+
+  applyEventBenefit(orders, totalPrice) {
+    const visitDate = this.#visitDate.getVisitDate();
+    const benefit = new Benefit();
+    const giftDetails = benefit.getGiftDetails(totalPrice);
+    const discountDetails = benefit.getDiscountDetails(orders, visitDate);
+    const totalBenefitDetails = giftDetails.concat(discountDetails);
+    const totalBenefitAmount = benefit.getTotalBenefitAmount(totalBenefitDetails);
+
+    OutputView.printGiftMenu(giftDetails);
+    OutputView.pritnBenefitDetails(totalBenefitDetails);
+    OutputView.printTotalBenefitAmount(totalBenefitAmount);
+    OutputView.printTotalAmountAfterDiscount(
+      totalPrice - benefit.getTotalDiscountAmount(discountDetails),
+    );
+    OutputView.printEventBadge(benefit.getBadgeDetail(totalBenefitAmount));
+  }
+}
+
+export default EventPlanner;
