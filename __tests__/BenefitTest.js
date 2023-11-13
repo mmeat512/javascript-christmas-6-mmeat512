b7952608489fa4a75c96c502777b6d8609dae6a0
mmeat512 <wndus0958@naver.com>
2023-11-14 00:54:48 +0900

    test: 증정 혜택 상세 테스트 추가

--- __tests__/BenefitTest.js
+++ __tests__/BenefitTest.js
@@ -1,7 +1,7 @@
 import Benefit from '../src/domain/Benefit';
 
 describe('혜택 테스트', () => {
-  test('할인 혜택 상세를 얻을 수 있다.', () => {
+  test('할인 혜택 상세리스트를 얻는다.', () => {
     // given
     const DATE = 6;
     const ORDER = {
@@ -20,10 +20,71 @@ describe('혜택 테스트', () => {
     ];
 
     // when
-    const benefit = new Benefit(ORDER, DATE);
-    const result = benefit.getDiscountDetails();
+    const benefit = new Benefit();
+    const result = benefit.getDiscountDetails(ORDER, DATE);
 
     // then
     expect(result).toStrictEqual(DISCOUNT_DETAILS);
+  });
+
+  test.each([
+    [120000, [{ name: '증정 이벤트', amount: 25000 }]],
+    [10000, []],
+  ])('[%s] 증정 혜택 상세리스트를 얻는다.', (totalPrice, expected) => {
+    // when
+    const benefit = new Benefit();
+    const result = benefit.getGiftDetails(totalPrice);
+
+    // then
+    expect(result).toStrictEqual(expected);
+  });
+
+  test('총혜택 금액을 얻는다.', () => {
+    // given
+    const BENEFIT_DETAILS = [
+      {
+        name: '크리스마스 디데이 할인',
+        amount: 1200,
+      },
+      {
+        name: '평일 할인',
+        amount: 4046,
+      },
+      {
+        name: '특별 할인',
+        amount: 1000,
+      },
+      {
+        name: '증정 이벤트',
+        amount: 25000,
+      },
+    ];
+    const TOTALB_BENEFIT_AMOUNT = 31246;
+
+    // when
+    const benefit = new Benefit();
+    const result = benefit.getTotalBenefitAmount(BENEFIT_DETAILS);
+
+    // then
+    expect(result).toBe(TOTALB_BENEFIT_AMOUNT);
+  });
+
+  test.each([
+    [31246, 'santa'],
+    [15000, 'tree'],
+    [5000, 'star'],
+  ])('[%d, %s] 총혜택 금액에 따라 배지 상세를 얻는다.', (totalBenefitAmount, badge) => {
+    // given
+    const SANTA_BADGE = {
+      name: '12월 이벤트 배지',
+      amount: badge,
+    };
+
+    // when
+    const benefit = new Benefit();
+    const result = benefit.getBadgeDetail(totalBenefitAmount);
+
+    // then
+    expect(result).toStrictEqual(SANTA_BADGE);
   });
 });
