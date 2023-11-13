87eaa25fc13b0f1349185ada3a359f7045fada28
mmeat512 <wndus0958@naver.com>
2023-11-14 00:54:18 +0900

    feat: 증정 혜택 상세 기능 구현

---
+++ src/constants/benefit.js
@@ -0,0 +1,9 @@
+export const BENEFIT_NAME = Object.freeze({
+  christmasDDay: '크리스마스 디데이 할인',
+  special: '특별 할인',
+  weekday: '평일 할인',
+  weekend: '주말 할인',
+  gift: '증정 이벤트',
+});
+
+export const DECEMBER_BADGE = '12월 이벤트 배지';
--- src/domain/Benefit.js
+++ src/domain/Benefit.js
@@ -1,44 +1,52 @@
-import { WEEKEND, WEEK_DAY } from '../constants/date.js';
-import { DISCOUNT_TYPE, SPECIAL_DAY, NO_DISCOUNT } from '../constants/discount.js';
+import { SPECIAL_DAY, NO_DISCOUNT } from '../constants/discount.js';
+import { MENU } from '../constants/menu.js';
 import Discount from './Discount.js';
 import Gift from './Gift.js';
+import { BENEFIT_NAME, DECEMBER_BADGE } from '../constants/benefit.js';
 
 class Benefit {
-  #order;
-  #date;
-
-  constructor(order, date) {
-    this.#order = order;
-    this.#date = date;
-  }
-
-  getDiscountDetails() {
-    const discount = new Discount(this.#date);
+  getDiscountDetails(order, date) {
+    const discount = new Discount(date);
     const discountDetail = [];
-    const everydayAmount = discount.getEverydayAmount(this.#order);
+    const everydayAmount = discount.getEverydayAmount(order);
     const ddayAmount = discount.getChristmasDDayAmount();
     const specialAmount = discount.getSpecialDayAmout(SPECIAL_DAY);
 
     if (NO_DISCOUNT !== everydayAmount.amount) discountDetail.push(everydayAmount);
     if (NO_DISCOUNT !== ddayAmount)
-      discountDetail.push(this.generateDiscountDetail('크리스마스 디데이 할인', ddayAmount));
+      discountDetail.push(this.#generateBenefitDetail(BENEFIT_NAME.christmasDDay, ddayAmount));
     if (NO_DISCOUNT !== specialAmount)
-      discountDetail.push(this.generateDiscountDetail('특별 할인', specialAmount));
+      discountDetail.push(this.#generateBenefitDetail(BENEFIT_NAME.special, specialAmount));
 
     return discountDetail;
   }
 
-  generateDiscountDetail(name, amount) {
+  getGiftDetails(totalPrice) {
+    const gift = new Gift();
+    const giftDetails = [];
+
+    if (gift.canReciveChampagne(totalPrice))
+      giftDetails.push(this.#generateBenefitDetail(BENEFIT_NAME.gift, MENU.beverage.champagne));
+
+    return giftDetails;
+  }
+
+  getTotalBenefitAmount(benefitDetails) {
+    return benefitDetails.reduce((acc, benefitDetails) => acc + benefitDetails.amount, NO_DISCOUNT);
+  }
+
+  getBadgeDetail(totalBenefitAmount) {
+    const gift = new Gift();
+    const badge = gift.getReciveBadge(totalBenefitAmount);
+
+    return this.#generateBenefitDetail(DECEMBER_BADGE, badge);
+  }
+
+  #generateBenefitDetail(name, amount) {
     return {
       name: name,
       amount: amount,
     };
-  }
-
-  getTotalBenefitAmount(discountDetail) {}
-
-  getGiftDetail() {
-    const gift = new Gift();
   }
 }
 
