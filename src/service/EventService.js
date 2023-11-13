4ff2d9f7b5618843ee940ffa3824988738a5e81c
mmeat512 <wndus0958@naver.com>
2023-11-14 05:47:23 +0900

    fix: 유효값 검증 버그 수정

--- src/service/EventService.js
+++ src/service/EventService.js
@@ -6,26 +6,47 @@ import EventPlanner from './EventPlanner.js';
 import { EVENT_RANGE, MIN_ORDER_AMOUNT } from '../constants/event.js';
 
 class EventService {
-  async getInputValue() {
-    OutputView.printGreeting();
+  async getValidVisitDate() {
     while (true) {
       try {
         const visitDate = new VisitDate(await InputView.readDate());
-        const order = new Order(await InputView.readOrder());
 
-        OutputView.printPreviewGuide(visitDate);
-        return {
-          visitDate,
-          order,
-        };
+        return visitDate;
       } catch (error) {
-        OutputView.printError(error.message);
+        OutputView.printError(error);
       }
     }
   }
 
+  async getValidOrder() {
+    while (true) {
+      try {
+        const order = new Order(await InputView.readOrder());
+
+        return order;
+      } catch (error) {
+        OutputView.printError(error);
+      }
+    }
+  }
+
+  async getInputValue() {
+    OutputView.printGreeting();
+
+    const visitDate = await this.getValidVisitDate();
+    const order = await this.getValidOrder();
+
+    OutputView.printPreviewGuide(visitDate.getVisitDate());
+
+    return {
+      visitDate,
+      order,
+    };
+  }
+
   async getDecemberEventPlanner() {
     const { visitDate, order } = await this.getInputValue();
+
     const eventPlanner = new EventPlanner(visitDate, order);
 
     eventPlanner.getEventPlanner(EVENT_RANGE, MIN_ORDER_AMOUNT);
--- src/utils/utils.js
+++ src/utils/utils.js
@@ -1,5 +1,4 @@
 export const daysInMonth = (year, month) => new Date(year, month, 0).getDate();
 export const numberFormatWon = (number) => {
-  console.log(number);
   return number.toString().replace(/\B(?=(\d{3})+(?!\d))/g, ',');
 };
